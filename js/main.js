fe8e93ddc0bec507d3b433f0dfb8ae96df99c654
Maksym Soloviov <95883405+Makssol1208@users.noreply.github.com>
2022-07-31 20:56:39 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,4 @@
+document.querySelector('.menu-icon-wrapper').onclick = function(){
+document.querySelector('.menu-icon').classList.toggle('menu-icon-active');
+document.querySelector('.nav_list-holder').classList.toggle('nav_list-holder--visible');
+}
